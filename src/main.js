a242f81c9e931f2047b8438ea469d6a48c96a400
nabee <nabeelbaghoor@gmail.com>
2021-12-24 13:54:16 +0500

    .

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './Mystyle.css';
-import 'bootstrap' 
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./Mystyle.css";
+import "bootstrap";
 
-import router from './router'
+import router from "./router";
 
-
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount("#app");
