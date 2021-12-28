0f1b45b2114dd56e321310d9ee4d007e402ae18d
Nabeel Hassan <60441195+nabeelbaghoor@users.noreply.github.com>
2021-12-28 13:33:36 +0500

    ..

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import { createApp } from "vue";
+// import { createApp, use } from "vue";
+import * as Vue from "vue";
 import App from "./App.vue";
 import "./Mystyle.css";
 import "bootstrap";
 
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+// import vueScript2 from "vue-script2";
+// import Ads from "vue-google-adsense";
+// require("vue-script2");
+// Ads.Adsense;
+// use(Ads.Adsense);
+
+Vue.createApp(App).use(router).mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,33 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHashHistory } from "vue-router";
+// import Home from "../views/Home.vue";
+import Developers from "../views/Developers.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Developers,
   },
   {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/About.vue')
+    path: "/about",
+    name: "About",
+    component: () => import("../views/About.vue"),
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: () => import('../views/Login.vue')
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/Login.vue"),
   },
   {
-    path: '/developers',
-    name: 'Developers',
-    component: () => import('../views/Developers.vue')
-  }
-
-]
+    path: "/developers",
+    name: "Developers",
+    component: () => import("../views/Developers.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
